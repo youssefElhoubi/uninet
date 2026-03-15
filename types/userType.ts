ffdd81d26e11f9dfd226d306c29381f91243f24f
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-03-15 22:05:53 +0000

    9adit user type

---
+++ types/userType.ts
@@ -0,0 +1,10 @@
+export interface userType  {
+    name:string
+    level:number
+    email:string
+    password:string
+    class:string//utill I add the class type
+    promotion:string
+    campus:string
+    image:string
+}
